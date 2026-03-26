dabc0e1397c9ad0d9464b892e0560ae6bdf3ef4e
smartpress00-del <smart.press00@gmail.com>
2026-03-26 01:05:22 +0100

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,1 +1,12 @@
+// middleware/errorHandler.js
+function errorHandler(err, req, res, next) {
+  console.error(err.stack); // Logs error for debugging
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Something went wrong on the server",
+  });
+}
+
+module.exports = errorHandler;
 
