e281d588ba2a1f73ef7da84d2bdde652004f2509
smartpress00-del <smart.press00@gmail.com>
2026-03-26 01:36:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const loanRoutes = require('./routes/loan');
 app.use('/api/auth', authRoutes);
 app.use('/api/wallet', walletRoutes);
 app.use('/api/loan', loanRoutes);
-const PORT = process.env.PORT || 5000;
 
+apps.use(errorHandler); // must come last
+
+const PORT = process.env.PORT || 5000;
 app.listen(5000, () => console.log("Server running on port 5000"));
-app.use(errorHandler); // must come last
+
