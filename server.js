b66b255835dd41c4fed44c000602f64f39ebc370
Isaiah avwerosuo <smart.press00@gmail.com>
2026-03-30 21:22:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const app = express();
 
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("DB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 // Routes
 const authRoutes = require('./routes/auth');
