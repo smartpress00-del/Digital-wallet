637f293c5cf8e78eed1e4afbc4d37f277bcf6806
Isaiah avwerosuo <smart.press00@gmail.com>
2026-03-31 15:54:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("DB Connected"))
+  .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.error("MongoDB connection error:", err));
 
 // Routes
